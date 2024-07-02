8fed78430142ec808227dfd91de001b325c6d88d
ryo.tanaka <ryo.tanaka@bizreach.co.jp>
2024-07-02 16:04:09 +0900

    Step01をコミット

--- src/test/java/org/docksidestage/javatry/basic/Step01VariableTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step01VariableTest.java
@@ -14,10 +14,13 @@
  * governing permissions and limitations under the License.
  */
 package org.docksidestage.javatry.basic;
+//TODO tanaka org.docksidestage.javatry.basicパッケージを定義
 
 import java.math.BigDecimal;
+//TODO tanaka java.math.BigDecimalクラスをインポート。以降はBigDecimalで呼び出せる。
 
 import org.docksidestage.unit.PlainTestCase;
+//TODO tanaka org.docksidestage.unit.PlainTestCaseクラスをインポート。以降はBigDecimalで呼び出せる。
 
 /**
  * The test of variable. <br>
@@ -27,7 +30,7 @@ import org.docksidestage.unit.PlainTestCase;
  * @author your_name_here
  */
 public class Step01VariableTest extends PlainTestCase {
-
+//TODO tanaka  Step01VariableTestクラスを定義。PlainTestCaseクラスを継承。アクセス修飾子publicをつけることで他パッケージからも利用可能に。
     // ===================================================================================
     //                                                                      Local Variable
     //                                                                      ==============
@@ -39,6 +42,9 @@ public class Step01VariableTest extends PlainTestCase {
         String sea = "mystic";
         log(sea); // your answer? => mystic
     }
+    //TODO tanaka test_variable_basicメソッドを定義。引数はなし。voidにより戻り値のないメソッドの型に。
+    //TODO tanaka Stringは文字列を表す型。
+    //TODO tanaka logメソッドはPlainTestCase内で定義。protected修飾子をつけて定義しているため、PlainTestCaseクラスを継承したStep01VariableTestクラスでも利用可能。
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_variable_initial() {
@@ -47,17 +53,20 @@ public class Step01VariableTest extends PlainTestCase {
         String piari = null;
         String dstore = "mai";
         sea = sea + land + piari + ":" + dstore;
-        log(sea); // your answer? => 
+        log(sea); // your answer? => mystic8null:mai
     }
-
+    //TODO tanaka 文字列との結合の場合、数値、nullは文字列("")に変換される？
+    //TODO tanaka Integlerはクラス型で整数値を保持する。Integler.~~でメソッドを呼び出せる。またnullを代入できる。intはプリミティブ型で整数値を保持し、nullは代入できない。
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_variable_reassigned_basic() {
         String sea = "mystic";
         String land = "oneman";
         sea = land;
         land = land + "'s dreams";
-        log(sea); // your answer? => 
+        log(sea); // your answer? => oneman
     }
+    //TODO tanaka 変数に変数を代入することができる。今回の場合、sea="oneman"？また59行目に sea = land;を追加すればoneman's dreamsとなる？
+    //TODO tanaka 型を定義したら、2回目以降変数名のみで良い？
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_variable_reassigned_int() {
@@ -65,8 +74,10 @@ public class Step01VariableTest extends PlainTestCase {
         int land = 415;
         sea = land;
         land++;
-        log(sea); // your answer? => 
+        log(sea); // your answer? => 415
     }
+    //TODO tanaka 変数に変数を代入できるが、seaが保持するのはlandではなく415?
+    //TODO tanaka land++はlandに1を足す
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_variable_reassigned_BigDecimal() {
@@ -75,8 +86,11 @@ public class Step01VariableTest extends PlainTestCase {
         sea = land;
         sea = land.add(new BigDecimal(1));
         sea.add(new BigDecimal(1));
-        log(sea); // your answer? => 
+        log(sea); // your answer? => 417
     }
+    //TODO tanaka BigDecimalは小数点以下を正確に扱うクラス。new演算子でクラスをインスタンス化。基本構文はクラス名 インスタンス名 = new コンストラクタ名([引数])。
+    //TODO tanaka addメソッドはBigDecimalファイルで定義。
+    //TODO tanaka 87行目で1を足している？88行目も1を足している？
 
     // ===================================================================================
     //                                                                   Instance Variable
@@ -85,33 +99,37 @@ public class Step01VariableTest extends PlainTestCase {
     private int instanceDockside;
     private Integer instanceHangar;
     private String instanceMagiclamp;
+    //TODO tanaka 変数を4つ定義
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_variable_instance_variable_default_String() {
         String sea = instanceBroadway;
-        log(sea); // your answer? => 
+        log(sea); // your answer? => null
     }
-
+    //TODO tanaka 変数instanceBroadwayに値を代入していないためnullとなる
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_variable_instance_variable_default_int() {
         int sea = instanceDockside;
-        log(sea); // your answer? => 
+        log(sea); // your answer? => 0
     }
+    //TODO tanaka 変数instanceDocksideに値を代入しておらず、かつintはプリミティブ型でnullを入れることはできないため0となる
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_variable_instance_variable_default_Integer() {
         Integer sea = instanceHangar;
-        log(sea); // your answer? => 
+        log(sea); // your answer? => null
     }
-
+    //TODO tanaka Integerはクラス型でnullを入れることができるためnullとなる
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_variable_instance_variable_via_method() {
         instanceBroadway = "bbb";
         instanceMagiclamp = "magician";
         helpInstanceVariableViaMethod(instanceMagiclamp);
         String sea = instanceBroadway + "|" + instanceDockside + "|" + instanceHangar + "|" + instanceMagiclamp;
-        log(sea); // your answer? => 
+        log(sea); // your answer? => bigband|1|null|burn
     }
+    //TODO tanaka メソッドや変数を後に定義しても問題なし。
+    //TODO tanaka　なぜburnではなく、magician?
 
     private void helpInstanceVariableViaMethod(String instanceMagiclamp) {
         instanceBroadway = "bigband";
@@ -130,14 +148,19 @@ public class Step01VariableTest extends PlainTestCase {
         String sea = "harbor";
         int land = 415;
         helpMethodArgumentImmutableMethodcall(sea, land);
-        log(sea); // your answer? => 
+        log(sea); // your answer? => harbor416
     }
+    //TODO tanaka 416いらなかった。seaはあくまで結合元でsea=harbor416とはなっていない。
+    //TODo tanaka helpMethodArgumentImmutableMethodcallメソッドにlog(sea.concat(landStr));を追加したところ、harbor416が表示された
 
     private void helpMethodArgumentImmutableMethodcall(String sea, int land) {
         ++land;
         String landStr = String.valueOf(land); // is "416"
         sea.concat(landStr);
     }
+    //TODO tanaka String.valueOfでstring型に変換
+    //TODO tanaka concatメソッドは、引数で指定した文字列を「結合対象の文字列の最後に結合」するためのメソッド。
+    //TODO tanaka sea.concat(landStr)は文字列seaの最後に文字列landstrを結合
 
     // -----------------------------------------------------
     //                                   Mutable Method-call
@@ -145,10 +168,16 @@ public class Step01VariableTest extends PlainTestCase {
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_variable_method_argument_mutable_methodcall() {
         StringBuilder sea = new StringBuilder("harbor");
+        //TODO tanaka StringBuilder クラスのインスタンスを作成
         int land = 415;
         helpMethodArgumentMethodcall(sea, land);
-        log(sea); // your answer? => 
+        log(sea); // your answer? => harbor416
     }
+    //TODO tanaka StringBuilderで複数の文字列や他の値を連結し、最後に文字列として出力する
+    //TODO tanaka 当たった。
+    //TODO tanaka concatメソッドは元になる文字列自体を変更させるものではなく、「結合した新しいインスタンスを生む」
+    //TODO tanaka appendメソッドはインスタンスを生成させず、元になる文字列自体を変更させる。そのためもともとインスタンスが用意されている状態で使う。
+    //TODO tanaka appendメソッドではStringBuilderクラス、StringBufferクラスのいずれかを用いてインスタンスを作成。
 
     private void helpMethodArgumentMethodcall(StringBuilder sea, int land) {
         ++land;
@@ -163,15 +192,17 @@ public class Step01VariableTest extends PlainTestCase {
         StringBuilder sea = new StringBuilder("harbor");
         int land = 415;
         helpMethodArgumentVariable(sea, land);
-        log(sea); // your answer? => 
+        log(sea); // your answer? => harbor
     }
-
+    //TODO tanaka 当たったけど、よくわからない。下のメソッドに log(sea);を追加したらharbor416と表示された。
     private void helpMethodArgumentVariable(StringBuilder sea, int land) {
         ++land;
         String seaStr = sea.toString(); // is "harbor"
+        //TODO tanaka　自作クラスでto.stringメソッドを用いている。
         sea = new StringBuilder(seaStr).append(land);
+        //TODO tanaka
     }
-
+    //TODO tanaka toStringは文字列に変換するメソッド。nullを変換元に入れるとエラーが発生する。ここがvalueofメソッドとの違い
     // ===================================================================================
     //                                                                           Challenge
     //                                                                           =========
@@ -191,9 +222,15 @@ public class Step01VariableTest extends PlainTestCase {
      * o すべての変数をlog()でカンマ区切りの文字列で表示
      * </pre>
      */
+    int piari;
     public void test_variable_writing() {
+        String sea = "mystic";
+        Integer land = null;
+        sea= sea +","+land+","+piari;
+        log(sea);
         // define variables here
     }
+    //TODO tanaka mystic,null,0と表示された
 
     // ===================================================================================
     //                                                                           Good Luck
@@ -203,12 +240,17 @@ public class Step01VariableTest extends PlainTestCase {
      * (変数についてあなたのオリジナルの質問形式のエクササイズを作ってみましょう)
      * <pre>
      * _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
-     * your question here (ここにあなたの質問を):
+     * (メソッド終了時のlog(sea)とlog(star)の中身は？):
      * 
      * _/_/_/_/_/_/_/_/_/_/
      * </pre>
      */
+    int sea =15;
     public void test_variable_yourExercise() {
-        // write your code here
+        int sea = sea;
+        log(sea);
+        // エラーが出る
+        // TODO tanaka インスタンス変数とローカル変数が同一の場合、インスタンス変数にアクセスするにはthis.をつける。
+        // TODo tanaka this.をつけていないためここでは自身を参照代入している状態
     }
 }
