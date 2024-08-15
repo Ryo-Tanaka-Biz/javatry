c037da246b04c72f33d4f7ea33d49d394048cac7
ryo.tanaka <ryo.tanaka@bizreach.co.jp>
2024-08-15 11:13:00 +0900

    TODo→doneに修正

--- src/main/java/org/docksidestage/bizfw/basic/buyticket/TicketBuyResult.java
+++ src/main/java/org/docksidestage/bizfw/basic/buyticket/TicketBuyResult.java
@@ -1,6 +1,6 @@
 package org.docksidestage.bizfw.basic.buyticket;
 
-// TODO tanaryo タグコメントの上 (クラス宣言の直下) に一行空行を by jflute (2024/08/05)
+// done tanaryo タグコメントの上 (クラス宣言の直下) に一行空行を by jflute (2024/08/05)
 /**
  * @author tanaryo
  */
@@ -9,7 +9,7 @@ public class TicketBuyResult {
     //                                                                           Attribute
     //                                                                           =========
     // done tanaryo daycountがfinal付いてない理由がない by jflute (2024/08/05)
-    // TODO tanaryo ここではdaycountだけどTicketではdayCountなので一貫性がない by jflute (2024/08/05)
+    // done tanaryo ここではdaycountだけどTicketではdayCountなので一貫性がない by jflute (2024/08/05)
     private final int displayPrice; // written on ticket, park guest can watch this
     private final int change;
     private final Integer dayCount;
