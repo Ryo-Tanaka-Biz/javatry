f8df5d0896991c785c6545a047297796a21f783e
ryo.tanaka <ryo.tanaka@bizreach.co.jp>
2024-09-08 12:16:01 +0900

    インターフェースにjavadocを追加

--- src/main/java/org/docksidestage/bizfw/basic/buyticket/PresentTime.java
+++ src/main/java/org/docksidestage/bizfw/basic/buyticket/PresentTime.java
@@ -2,6 +2,10 @@ package org.docksidestage.bizfw.basic.buyticket;
 
 import java.time.LocalTime;
 
+/**
+ * 現在時刻を取得するインターフェース。
+ * @author tanaryo
+ */
 public interface PresentTime {
     public LocalTime getPresentTime();
 }
