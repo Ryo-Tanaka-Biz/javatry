dbc29a8e67da09419c6bde42f3bcd732d7bb2398
ryo.tanaka <ryo.tanaka@bizreach.co.jp>
2024-09-08 12:18:04 +0900

    現在時刻を取得するクラスにjavadocを追加

--- src/main/java/org/docksidestage/bizfw/basic/buyticket/DefaultPresentTime.java
+++ src/main/java/org/docksidestage/bizfw/basic/buyticket/DefaultPresentTime.java
@@ -2,6 +2,10 @@ package org.docksidestage.bizfw.basic.buyticket;
 
 import java.time.LocalTime;
 
+/**
+ * 現在時刻を取得するクラス。
+ * @author tanaryo
+ */
 public class DefaultPresentTime implements PresentTime {
     @Override
     public LocalTime getPresentTime() {
--- src/main/java/org/docksidestage/bizfw/basic/buyticket/VariablePresentTime.java
+++ src/main/java/org/docksidestage/bizfw/basic/buyticket/VariablePresentTime.java
@@ -2,6 +2,10 @@ package org.docksidestage.bizfw.basic.buyticket;
 
 import java.time.LocalTime;
 
+/**
+ * 意図的に変更した現在時刻を取得するクラス。
+ * @author tanaryo
+ */
 public class VariablePresentTime implements PresentTime {
 
     public final LocalTime startTime;
