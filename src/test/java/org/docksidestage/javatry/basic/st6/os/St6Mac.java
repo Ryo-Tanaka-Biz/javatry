32f67f32c538d1eee555fccb8b5bb92ceb429d58
ryo.tanaka <ryo.tanaka@bizreach.co.jp>
2024-11-27 17:13:19 +0900

    doneに変更

--- src/test/java/org/docksidestage/javatry/basic/st6/os/St6Mac.java
+++ src/test/java/org/docksidestage/javatry/basic/st6/os/St6Mac.java
@@ -6,7 +6,7 @@ import org.docksidestage.javatry.basic.st6.os.st6OperationSystem.St6OperationSys
  * @author tanaryo
  */
 public class St6Mac extends St6OperationSystem {
-    // TODO tanaryo osTypeズレる問題は？選択肢A,Bを出してもらった、Aの延長のCは？ by jflute (2024/11/15)
+    // TODO  done tanaryo osTypeズレる問題は？選択肢A,Bを出してもらった、Aの延長のCは？ by jflute (2024/11/15)
     // A. そもそもコンストラクターで受け取らず、newされたら固定のosTypeを自分でsetする
     // B. バリデーションでズレチェック
     // C. "A"の続きで、もうosTypeの唯一の目的であるバリデーションが不要なのでosType自体が不要
