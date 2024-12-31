7af4eb6ba3d43825c38fc426f2a932ea323ab72f
ryo.tanaka <ryo.tanaka@bizreach.co.jp>
2024-12-31 20:50:52 +0900

    TODOをdoneにする

--- src/test/java/org/docksidestage/javatry/basic/Step07ExceptionTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step07ExceptionTest.java
@@ -200,10 +200,9 @@ public class Step07ExceptionTest extends PlainTestCase {
             String sea = file.getCanonicalPath();
             log(sea);
         } catch (IOException e) {
-            // TODO tanaryo メッセージとスタックトレース出すときは、log(e)だけでOK by jflute (2024/12/26)
+            // TODO done tanaryo メッセージとスタックトレース出すときは、log(e)だけでOK by jflute (2024/12/26)
             // log()メソッドの中で、eが来たら、両方出すように作られているので。(これは知らなくて当然)
-            log(e.getMessage());
-            log(e.getStackTrace());
+            log(e);
         }
         // [1on1でのふぉろー] チェック例外と非チェック例外のお話少し。
         // 理想的にはチェック例外は、catchを促してくれる良い文法ではあるが...実際使われていない(ことがほとんど)
@@ -270,7 +269,7 @@ public class Step07ExceptionTest extends PlainTestCase {
     // => 根本原因を知るため by tanaryo
     // 例外が例外を保持できる理由について話した。
 
-    // TODO tanaryo [読み物課題] エラーメッセージ読め読め大合唱 by jflute (2024/12/26)
+    // TODO done tanaryo [読み物課題] エラーメッセージ読め読め大合唱 by jflute (2024/12/26)
     // https://jflute.hatenadiary.jp/entry/20130522/errorsinging
 
     // TODO jflute 次回ここから1on1 (2024/12/26)
